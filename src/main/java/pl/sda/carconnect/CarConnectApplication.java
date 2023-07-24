bcad8397dff853f6ded05fead4fcc0da315d1efd
Tomasz Szyperek <tomek.szyperek@gmail.com>
2023-07-24 18:32:07 +0200

    init commit

---
+++ src/main/java/pl/sda/carconnect/CarConnectApplication.java
@@ -0,0 +1,13 @@
+package pl.sda.carconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarConnectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarConnectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sda/carconnect/CarConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sda.carconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarConnectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
