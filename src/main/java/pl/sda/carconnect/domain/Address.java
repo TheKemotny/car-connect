ed0fecc1f4444bcfa14fa1028f31611eb064890f
Tomasz Szyperek <tomek.szyperek@gmail.com>
2023-08-07 18:57:23 +0200

    Address is Entity now

--- src/main/java/pl/sda/carconnect/domain/Address.java
+++ src/main/java/pl/sda/carconnect/domain/Address.java
@@ -1,11 +1,23 @@
 package pl.sda.carconnect.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ADDRESSES")
 public class Address {
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
     String street;
     String zipCode;
     String city;
